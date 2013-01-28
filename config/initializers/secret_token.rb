1a609931859c6f99dc85711bed49b45174512384
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-01-28 15:55:01 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PromoSite::Application.config.secret_token = '224262de17208c2a589a4eff3696d3b8acf03834e03464436e52cf2c3c8988b9d90257bcba6b3d27273f178ac0dcc8bea72a9f7db9bff27bdf3a6399a3e10f91'
