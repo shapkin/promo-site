ddc80bcdee7c9e1fba34b3977a1ea39251236d9a
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-02-20 15:31:04 +0400

    add services list

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
+  has_many :services
+
   attr_accessible :name
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,5 @@
 class Service < ActiveRecord::Base
+  belongs_to :category
+
   attr_accessible :description, :name, :photo, :price
 end
