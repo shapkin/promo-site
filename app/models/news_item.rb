f752e833a71ea544af3d209b17160901b4c5db5d
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-04-05 12:25:59 +0400

    add validation for news

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,3 +1,7 @@
 class NewsItem < ActiveRecord::Base
   attr_accessible :title, :body
+
+  validates :title, presence: true,
+                    length: { maximum: 250 }
+  validates :body,  presence: true
 end
