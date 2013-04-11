a47bdebffb2a2407c316458c51c548dc16356285
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-04-11 16:27:46 +0400

    add add new feedback form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.effect-highlight
 //= require bootstrap-dropdown.js
 //= require_tree .
--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,6 +1,21 @@
 class FeedbacksController < ApplicationController
   include ApplicationHelper
 
+  def create
+    @feedback = current_user.feedbacks.build(params[:feedback])
+    @feedback.save
+    respond_to do |format|
+      format.html do
+        unless @feedback.new_record?
+          redirect_to feedbacks_path, flash: { success: "New Feedback added!" }
+        else
+          render :new
+        end
+      end
+      format.js
+    end
+  end
+
   def index
     @feedbacks = Feedback.all
     @feedbacks = @feedbacks.approved unless manager?
@@ -10,8 +25,5 @@ class FeedbacksController < ApplicationController
   end
 
   def destroy
-  end
-
-  def create
   end
 end
